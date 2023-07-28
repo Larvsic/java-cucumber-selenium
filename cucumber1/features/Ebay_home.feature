23262d55b2d56c85268943c2eb2ead9bbdbf601f
Luis Ramirez <luisramirez@MacBook-Air-de-Luis.local>
2023-07-28 12:27:59 -0600

    Initial commit, Maven project cucumber

---
+++ cucumber1/features/Ebay_home.feature
@@ -0,0 +1,10 @@
+
+@tag
+Feature: Ebay home page scenarios
+  @tag1
+  Scenario: Advanced Search link
+    Given I am on Ebay home page
+    When I click on advanced link
+    Then I navigate to Advanced search page
+
+
---
+++ cucumber1/src/test/java/test/EbayHome_Steps.java
@@ -0,0 +1,27 @@
+package test;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class EbayHome_Steps {
+	
+	@Given("I am on Ebay home page")
+	public void i_am_on_ebay_home_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I click on advanced link")
+	public void i_click_on_advanced_link() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("I navigate to Advanced search page")
+	public void i_navigate_to_advanced_search_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+}
